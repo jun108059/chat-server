8139761f4e777a9269ba804660b4b00ce2aec183
YoungJun Park <youngjun108059@gmail.com>
2022-06-15 02:12:35 +0900

    👔 도메인 : 채팅 메시지 전송 기능 추가

--- core/src/main/java/hexagonal/core/domain/Member.java
+++ core/src/main/java/hexagonal/core/domain/Member.java
@@ -18,4 +18,24 @@ public class Member {
     @Column(nullable = false, length = 100)
     private String name;
 
+    @Column(nullable = false, length = 30)
+    private String type;
+
+    /**
+     * 채팅 메시지 전송
+     *
+     * @param roomId 방 번호
+     * @param messageContent 메시지 내용
+     * @return 메시지 전송 완료 여부
+     */
+    public boolean sendMessage(Long roomId, String messageContent) {
+        Message message = Message.builder()
+                .senderId(this.id)
+                .roomId(roomId)
+                .content(messageContent)
+                .build();
+        // Kafka 메시지 발송
+        // Message 저장
+        return true;
+    }
 }
