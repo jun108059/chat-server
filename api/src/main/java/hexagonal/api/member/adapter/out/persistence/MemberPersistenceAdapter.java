6e368d1318450a445ca4fd84cfc769f9b73801a0
YoungJun Park <youngjun108059@gmail.com>
2022-06-22 23:37:00 +0900

    👔 회원 조회/INSERT 영속성 OUT Adapter(#3)

--- api/src/main/java/hexagonal/api/member/adapter/out/persistence/MemberPersistenceAdapter.java
+++ api/src/main/java/hexagonal/api/member/adapter/out/persistence/MemberPersistenceAdapter.java
@@ -1,4 +1,31 @@
 package hexagonal.api.member.adapter.out.persistence;
 
-public class MemberPersistenceAdapter {
+import hexagonal.api.member.application.port.out.FindMemberPort;
+import hexagonal.api.member.application.port.out.SaveMemberPort;
+import hexagonal.core.domain.jpa.MemberJpaEntity;
+import hexagonal.core.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+
+@RequiredArgsConstructor
+@Component
+class MemberPersistenceAdapter implements FindMemberPort, SaveMemberPort {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberJpaEntity findMember(String email) {
+        MemberJpaEntity member = memberRepository.findByEmail(email)
+                .orElseThrow(EntityNotFoundException::new);
+        return member;
+    }
+
+
+    @Override
+    public Long saveMember(MemberJpaEntity memberJpaEntity) {
+        MemberJpaEntity saved = memberRepository.save(memberJpaEntity);
+        return saved.getId();
+    }
 }
