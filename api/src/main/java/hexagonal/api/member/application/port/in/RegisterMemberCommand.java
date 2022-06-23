e2a2480ad328d0b508e62a40455c9f84ec440b3f
YoungJun Park <youngjun108059@gmail.com>
2022-06-23 12:40:19 +0900

    👔 회원 가입 IN port Command(#3)

--- api/src/main/java/hexagonal/api/member/application/port/in/RegisterMemberCommand.java
+++ api/src/main/java/hexagonal/api/member/application/port/in/RegisterMemberCommand.java
@@ -1,15 +1,30 @@
 package hexagonal.api.member.application.port.in;
 
+import hexagonal.core.domain.jpa.MemberJpaEntity;
+import lombok.Getter;
+
+@Getter
 public class RegisterMemberCommand {
 
     private final String name;
+    private final String email;
     private final String memberType;
 
-    public RegisterMemberCommand(String name, String type) {
+    public RegisterMemberCommand(String name, String email, String type) {
         this.name = name;
+        this.email = email;
         this.memberType = type;
         // MemberType ENUM String 일치 입력 유효성 검증
         // name Validation 추가
+    }
+
+    public MemberJpaEntity toJpaEntity() {
+        return MemberJpaEntity.builder()
+                .id(null)
+                .name(name)
+                .email(email)
+                .type(memberType)
+                .build();
     }
 
 }
