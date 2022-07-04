94c089046632bfd75cf532b786be7d191b7d1d72
YoungJun Park <youngjun108059@gmail.com>
2022-07-04 23:47:47 +0900

    👔️ 채팅방 생성 서비스 로직 추가(#5)

---
+++ api/src/main/java/hexagonal/api/room/application/service/CreateRoomService.java
@@ -0,0 +1,37 @@
+package hexagonal.api.room.application.service;
+
+import hexagonal.api.room.application.port.in.CreateRoomCommand;
+import hexagonal.api.room.application.port.in.CreateRoomUseCase;
+import hexagonal.api.room.application.port.out.FindRoomPort;
+import hexagonal.api.room.application.port.out.SaveRoomPort;
+import hexagonal.core.domain.jpa.RoomJpaEntity;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.EntityNotFoundException;
+
+@RequiredArgsConstructor
+public class CreateRoomService implements CreateRoomUseCase {
+
+    private final FindRoomPort findRoomPort;
+    private final SaveRoomPort saveRoomPort;
+
+    @Override
+    public Long createRoom(CreateRoomCommand command) {
+        // 중복 채팅방 검사
+        if (!checkRoomExists(command.getCompanyId())) {
+            RoomJpaEntity roomJpaEntity = command.toJpaEntity();
+            return saveRoomPort.saveRoom(roomJpaEntity);
+        }
+        return null;
+    }
+
+    private boolean checkRoomExists(Long companyId) {
+        try {
+            findRoomPort.findRoomByCompanyId(companyId);
+        } catch (EntityNotFoundException e) {
+            return false;
+        }
+
+        return true;
+    }
+}
