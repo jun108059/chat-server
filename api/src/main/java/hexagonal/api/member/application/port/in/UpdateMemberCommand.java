5e1237eb9c127f940d979951a0ca7b64bba0c6b1
YoungJun Park <youngjun108059@gmail.com>
2022-07-26 00:11:22 +0900

    👔 회원 정보 변경(in port)(#14)

--- api/src/main/java/hexagonal/api/member/application/port/in/UpdateMemberCommand.java
+++ api/src/main/java/hexagonal/api/member/application/port/in/UpdateMemberCommand.java
@@ -1,6 +1,5 @@
 package hexagonal.api.member.application.port.in;
 
-import hexagonal.core.domain.jpa.MemberJpaEntity;
 import lombok.Getter;
 
 @Getter
@@ -8,23 +7,12 @@ public class UpdateMemberCommand {
 
     private final Long id;
     private final String name;
-    private final String email;
     private final String memberType;
 
-    public UpdateMemberCommand(Long id, String name, String email, String type) {
+    public UpdateMemberCommand(Long id, String name, String type) {
         this.id = id;
         this.name = name;
-        this.email = email;
         this.memberType = type;
-    }
-
-    public MemberJpaEntity toJpaEntity() {
-        return MemberJpaEntity.builder()
-                .id(id)
-                .name(name)
-                .email(email)
-                .type(memberType)
-                .build();
     }
 
 }
