ad5ffe3a194bb777e6a1f14bdacb824132e62544
YoungJun Park <youngjun108059@gmail.com>
2022-07-30 02:27:12 +0900

    🔧 Swagger API 문서 에너테이션 적용(#16)

--- api/src/main/java/hexagonal/api/member/adapter/in/web/RegisterMemberController.java
+++ api/src/main/java/hexagonal/api/member/adapter/in/web/RegisterMemberController.java
@@ -4,6 +4,9 @@ import hexagonal.api.member.adapter.in.web.request.RegisterMemberReq;
 import hexagonal.api.member.adapter.in.web.response.RegisterMemberResponse;
 import hexagonal.api.member.application.port.in.RegisterMemberCommand;
 import hexagonal.api.member.application.port.in.RegisterMemberUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,13 @@ public class RegisterMemberController {
 
     private final RegisterMemberUseCase registerMemberUseCase;
 
+    @Operation(summary = "회원 가입 요청", description = "회원 데이터가 저장됩니다.", tags = { "Member Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping("/v1/member")
     ResponseEntity<RegisterMemberResponse> registerMember(@RequestBody RegisterMemberReq memberInfo) {
         // ToDo 입력모델 Valid Check 예외 처리
--- api/src/main/java/hexagonal/api/room/adapter/in/web/CreateRoomController.java
+++ api/src/main/java/hexagonal/api/room/adapter/in/web/CreateRoomController.java
@@ -4,6 +4,9 @@ import hexagonal.api.room.adapter.in.web.request.CreateRoomReq;
 import hexagonal.api.room.adapter.in.web.response.CreateRoomRes;
 import hexagonal.api.room.application.port.in.CreateRoomCommand;
 import hexagonal.api.room.application.port.in.CreateRoomUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,13 @@ public class CreateRoomController {
 
     private final CreateRoomUseCase createRoomUseCase;
 
+    @Operation(summary = "채팅방 생성 요청", description = "채팅방 데이터가 생성됩니다.", tags = { "Room Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping("/v1/room")
     ResponseEntity<CreateRoomRes> createRoom(@RequestBody CreateRoomReq roomReq) {
         CreateRoomCommand command = new CreateRoomCommand(
