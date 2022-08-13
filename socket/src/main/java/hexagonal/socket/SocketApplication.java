669751d090b7ec0e7c3d13aa543c13088110f181
YoungJun Park <youngjun108059@gmail.com>
2022-08-13 13:15:06 +0900

    🔧 Kafka 설정 Configuration 에너테이션 추가

--- socket/src/main/java/hexagonal/socket/SocketApplication.java
+++ socket/src/main/java/hexagonal/socket/SocketApplication.java
@@ -1,9 +1,12 @@
 package hexagonal.socket;
 
+import hexagonal.socket.kafka.config.KafkaConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {KafkaConstants.class})
 public class SocketApplication {
 
 	public static void main(String[] args) {
