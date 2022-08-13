704c7f8e7dfef2872d4916731a6bf87723ab1230
YoungJun Park <youngjun108059@gmail.com>
2022-08-13 13:14:18 +0900

    👔 Producer send 컨트롤러 추가

---
+++ socket/src/main/java/hexagonal/socket/controller/KafkaProducerController.java
@@ -0,0 +1,24 @@
+package hexagonal.socket.controller;
+
+import hexagonal.socket.dto.Message;
+import hexagonal.socket.kafka.service.ProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class KafkaProducerController {
+
+    private final ProducerService producerService;
+
+    @PostMapping(value = "/api/send", consumes = "application/json", produces = "application/json")
+    public void sendMessage(@RequestBody Message message) {
+        message.setTimestamp(LocalDateTime.now().toString());
+        producerService.sendMessage(message);
+    }
+
+}
