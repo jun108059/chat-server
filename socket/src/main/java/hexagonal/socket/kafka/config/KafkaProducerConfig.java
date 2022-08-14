176ef49eedc8dce96f67817c525abea46845c429
YoungJun Park <youngjun108059@gmail.com>
2022-08-14 23:24:53 +0900

    🔧 Producer 설정 파일 추가 (#19)

---
+++ socket/src/main/java/hexagonal/socket/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,47 @@
+package hexagonal.socket.kafka.config;
+
+import hexagonal.socket.dto.Message;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * KafkaTemplate - Topic 으로 데이터 보내는 편의 기능 설정(By Producer)
+ */
+@EnableKafka
+@Configuration
+@RequiredArgsConstructor
+public class KafkaProducerConfig {
+
+    private final KafkaConstants kafkaConstants;
+
+    @Bean
+    public KafkaTemplate<String, Message> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, Message> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigurations());
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigurations() {
+        Map<String, Object> configurations = new HashMap<>();
+        configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConstants.getProducerBootStrapServers());
+        configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return configurations;
+    }
+
+}
